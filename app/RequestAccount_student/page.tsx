0e6d0968ab0018e38412da1c2b4ccb640dc30b24
zeyad <zeyad>
2025-07-11 15:49:36 -0400

    Add login and request account UI

---
+++ app/RequestAccount_student/page.tsx
@@ -0,0 +1,18 @@
+
+import './RequestAccount.css';
+
+export default function LoginOrRequestStudentPage() {
+  return (
+    <div className="Registration_container">
+      <header className="header">
+        <div className="logo">
+          <h1>🎓 University Portal - Student Registration</h1>
+        </div>
+      </header>
+
+      <main className="main-content">
+       
+      </main>
+    </div>
+  );
+}
