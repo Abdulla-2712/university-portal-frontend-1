76fe1af2a4230842e4ffd761b1613c6ab1478593
Abdulla Omar Ali <aabdula2712@gmail.com>
2025-08-17 16:07:33 +0300

    complaints interface needs declaration-2

--- Components/AdminComplaints.tsx
+++ Components/AdminComplaints.tsx
@@ -3,7 +3,15 @@ import React, { useEffect, useState } from 'react'
 import AdminComplaintsCard from './AdminComplaintsCard'
 
 export default function ComplaintsAdminTab() {
-const [complaints, setComplaints] = useState<Complaint[]>([])
+const [complaints, setComplaints] = useState<{
+  id: number
+  subject: string
+  status: string
+  department: string
+  priority_level: string
+  complaint_content: string
+  complaint_answer: string | null
+}[]>([])
 
   useEffect(() => {
     const token = localStorage.getItem('token');
